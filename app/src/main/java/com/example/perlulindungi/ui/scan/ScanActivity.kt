678a536829e08cbb3bb2f0b3c3e744c5d4cafb76
jonathan-cj <69293681+jonathan-cj@users.noreply.github.com>
2022-02-20 00:14:53 +0700

    add scan activity layout

---
+++ app/src/main/java/com/example/perlulindungi/ui/scan/ScanActivity.kt
@@ -0,0 +1,12 @@
+package com.example.perlulindungi.ui.scan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.perlulindungi.R;
+
+class ScanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+    }
+}
