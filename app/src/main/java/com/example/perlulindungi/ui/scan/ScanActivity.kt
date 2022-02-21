a12e3508977c2cf7a961164f8660af07f8c8be25
jonathan-cj <69293681+jonathan-cj@users.noreply.github.com>
2022-02-21 14:44:43 +0700

    open camera on scan activity

--- app/src/main/java/com/example/perlulindungi/ui/scan/ScanActivity.kt
+++ app/src/main/java/com/example/perlulindungi/ui/scan/ScanActivity.kt
@@ -1,12 +1,91 @@
 package com.example.perlulindungi.ui.scan
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import com.example.perlulindungi.R;
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.example.perlulindungi.databinding.ActivityScanBinding
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.ExecutionException
+
 
 class ScanActivity : AppCompatActivity() {
+    private val PERMISSION_REQUEST_CAMERA = 0
+    private lateinit var scanBinding: ActivityScanBinding
+    private lateinit var previewView: PreviewView
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan)
+        scanBinding = ActivityScanBinding.inflate(layoutInflater)
+        setContentView(scanBinding.root)
+
+        previewView = scanBinding.scanPreviewView
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        requestCamera()
     }
+
+    private fun requestCamera() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            startCamera()
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), PERMISSION_REQUEST_CAMERA);
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), PERMISSION_REQUEST_CAMERA);
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun startCamera() {
+        cameraProviderFuture.addListener({
+            try {
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+                bindCameraPreview(cameraProvider)
+            } catch (e: ExecutionException) {
+                Toast.makeText(this, "Error starting camera " + e.message, Toast.LENGTH_SHORT)
+                    .show()
+            } catch (e: InterruptedException) {
+                Toast.makeText(this, "Error starting camera " + e.message, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun bindCameraPreview(cameraProvider: ProcessCameraProvider) {
+        val preview: Preview = Preview.Builder().build()
+        val cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+        preview.setSurfaceProvider(previewView.surfaceProvider)
+        val camera: Camera =
+            cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+    }
+
+
 }
