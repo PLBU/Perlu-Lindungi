982c5608b1c343be6aca61fd50ce77cfb8a6272e
jonathan-cj <69293681+jonathan-cj@users.noreply.github.com>
2022-02-24 17:32:30 +0700

    add barcode scanner

--- app/src/main/java/com/example/perlulindungi/ui/scan/ScanActivity.kt
+++ app/src/main/java/com/example/perlulindungi/ui/scan/ScanActivity.kt
@@ -1,91 +1,80 @@
 package com.example.perlulindungi.ui.scan
 
-import android.Manifest
-import android.content.pm.PackageManager
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.Camera
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.LifecycleOwner
 import com.example.perlulindungi.databinding.ActivityScanBinding
-import com.google.common.util.concurrent.ListenableFuture
-import java.util.concurrent.ExecutionException
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.ResultPoint
+import com.google.zxing.client.android.BeepManager
+import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.BarcodeResult
+import com.journeyapps.barcodescanner.DecoratedBarcodeView
+import com.journeyapps.barcodescanner.DefaultDecoderFactory
+import java.util.*
 
 
+//import me.dm7.barcodescanner.zxing.ZXingScannerView
+
+
+//class ScanActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
 class ScanActivity : AppCompatActivity() {
-    private val PERMISSION_REQUEST_CAMERA = 0
     private lateinit var scanBinding: ActivityScanBinding
-    private lateinit var previewView: PreviewView
-    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var barcodeView: DecoratedBarcodeView
+    private lateinit var lastText: String
+    private lateinit var beepManager: BeepManager
+
+    private val callback: BarcodeCallback = object : BarcodeCallback {
+        override fun barcodeResult(result: BarcodeResult) {
+            Log.d("RESULT", result.text)
+            if (result.text == null || result.text == lastText) {
+                // Prevent duplicate scans
+                return
+            }
+            lastText = result.text
+            barcodeView.setStatusText(result.text)
+            beepManager.playBeepSoundAndVibrate()
+
+//            //Added preview of scanned barcode
+//            val imageView = findViewById(R.id.barcodePreview)
+//            imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW))
+        }
+
+        override fun possibleResultPoints(resultPoints: List<ResultPoint>) {}
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         scanBinding = ActivityScanBinding.inflate(layoutInflater)
         setContentView(scanBinding.root)
 
-        previewView = scanBinding.scanPreviewView
+        lastText = ""
 
-        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-        requestCamera()
+        val intent: Intent = Intent("com.google.zxing.client.android.SCAN")
+        intent.putExtra("PROMPT_MESSAGE", "")
+
+        barcodeView = scanBinding.scanBarcodeView
+        val formats: Collection<BarcodeFormat> =
+        listOf(BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39)
+        barcodeView.barcodeView.decoderFactory = DefaultDecoderFactory(formats)
+        barcodeView.initializeFromIntent(intent)
+        barcodeView.decodeContinuous(callback)
+
+        scanBinding.scanBackBtn.setOnClickListener { onBackPressed() }
+
+        beepManager = BeepManager(this)
     }
 
-    private fun requestCamera() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-            startCamera()
-        } else {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), PERMISSION_REQUEST_CAMERA);
-            } else {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), PERMISSION_REQUEST_CAMERA);
-            }
-        }
+    override fun onResume() {
+        super.onResume()
+        barcodeView.resume()
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String?>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSION_REQUEST_CAMERA) {
-            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                startCamera()
-            } else {
-                Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show()
-            }
-        }
+    override fun onPause() {
+        super.onPause()
+        barcodeView.pause()
     }
-
-    private fun startCamera() {
-        cameraProviderFuture.addListener({
-            try {
-                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-                bindCameraPreview(cameraProvider)
-            } catch (e: ExecutionException) {
-                Toast.makeText(this, "Error starting camera " + e.message, Toast.LENGTH_SHORT)
-                    .show()
-            } catch (e: InterruptedException) {
-                Toast.makeText(this, "Error starting camera " + e.message, Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }, ContextCompat.getMainExecutor(this))
-    }
-
-    private fun bindCameraPreview(cameraProvider: ProcessCameraProvider) {
-        val preview: Preview = Preview.Builder().build()
-        val cameraSelector = CameraSelector.Builder()
-            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-            .build()
-        preview.setSurfaceProvider(previewView.surfaceProvider)
-        val camera: Camera =
-            cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
-    }
-
-
 }
