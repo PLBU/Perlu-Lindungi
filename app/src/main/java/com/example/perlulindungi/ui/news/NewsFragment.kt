d97acd99b078c5d862600a80d8d889de96f606f7
Renaldi Arlin <johanes123renaldi@gmail.com>
2022-03-03 11:57:22 +0700

    Add News Component for news list in NewsFragment.kt

--- app/src/main/java/com/example/perlulindungi/ui/news/NewsFragment.kt
+++ app/src/main/java/com/example/perlulindungi/ui/news/NewsFragment.kt
@@ -1,37 +1,51 @@
 package com.example.perlulindungi.ui.news
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.perlulindungi.databinding.FragmentNewsBinding
 
 class NewsFragment : Fragment() {
 
     private var _binding: FragmentNewsBinding? = null
+    private var _newsAdapter: NewsAdapter? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val newsAdapter get() = _newsAdapter!!
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this)[NewsViewModel::class.java]
-
         _binding = FragmentNewsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        _newsAdapter = NewsAdapter()
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val newsViewModel =
+            ViewModelProvider(this)[NewsViewModel::class.java]
+        val root: View = binding.root
+        val recyclerView: RecyclerView = binding.recyclerView
+
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        recyclerView.adapter = newsAdapter
+
+        newsViewModel.getAllNews()
+        newsViewModel.allNewsData?.observe(viewLifecycleOwner) {
+            if (it != null) {
+                newsAdapter.setNewsList(it)
+                newsAdapter.notifyDataSetChanged()
+            }
         }
+
         return root
     }
 
--- app/src/main/java/com/example/perlulindungi/ui/news/NewsViewModel.kt
+++ app/src/main/java/com/example/perlulindungi/ui/news/NewsViewModel.kt
@@ -3,11 +3,16 @@ package com.example.perlulindungi.ui.news
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.perlulindungi.data.news.NewsModel
+import com.example.perlulindungi.data.news.NewsRepo
 
 class NewsViewModel : ViewModel() {
+    var allNewsData: LiveData<List<NewsModel>>? = null
+    private var newsRepo: NewsRepo = NewsRepo()
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    fun getAllNews() {
+        if (allNewsData == null) {
+            allNewsData = newsRepo.requestAllNews()
+        }
     }
-    val text: LiveData<String> = _text
 }
